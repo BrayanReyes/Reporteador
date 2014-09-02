12237a84d9c112a18d908c80a7e0cd7d71ae5218
BrayanReyes <zadon_05@hotmail.com>
2014-09-02 13:44:24 -0500

    Funciones basicas

---
+++ Modelo/reporte.js
@@ -0,0 +1,6 @@
+function reporte (nombre, descripcion, atributos, informacion){
+	reporte.nombre : nombre,
+	reporte.descripcion : descripcion,
+	reporte.atributos: atributos,
+	reporte.informacion: informacion
+};
