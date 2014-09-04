b7a3a849bbddd67cb22242c8fded4abbacc561dc
BrayanReyes <zadon_05@hotmail.com>
2014-09-04 05:16:20 -0500

    test

--- Modelo/reporte.js
+++
@@ -1,6 +0,0 @@
-function reporte (nombre, descripcion, atributos, informacion){
-	reporte.nombre : nombre,
-	reporte.descripcion : descripcion,
-	reporte.atributos: atributos,
-	reporte.informacion: informacion
-};
